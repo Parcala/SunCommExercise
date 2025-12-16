3117d2d45cdc468f787d5ef432587039b6f92794
Parcala <72952302+Parcala@users.noreply.github.com>
2025-12-16 09:58:25 -0500

    Local Dev Work

--- SunCommunitiesExercise/Controllers/HealthController.cs
+++ SunCommunitiesExercise/Controllers/HealthController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using SunCommunitiesExercise.Models;
 using static SunCommunitiesExercise.Constants;
 
